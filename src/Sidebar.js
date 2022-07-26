eb0cc6795ea49c07e7792c8af840ea27413f9963
mohamedfazilk <2114921@cognizant.com>
2022-07-26 15:35:48 +0530

    set the close button and onclick function

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useGlobalContext } from './context';
+import { FaTimes } from 'react-icons/fa'
 
 const Sidebar = () => {
     const { isSidebarOpen, closeSidebar } = useGlobalContext();
     return (
         <aside className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
             }`}>
-            Sidebar
-        </aside>
+            <div className="sidebar">
+                <button className='close-btn'>
+                    <FaTimes />
+                </button>
+            </div>
+        </aside >
     )
 }
 
