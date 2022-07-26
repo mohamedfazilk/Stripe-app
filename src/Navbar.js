c8d5eb65467483336c02e0a9d4bf2cd5f1c4a547
mohamedfazilk <2114921@cognizant.com>
2022-07-26 13:48:55 +0530

    place all the links

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,9 +14,26 @@ const Navbar = () => {
             <button className='btn toggle-btn' onClick={openSidebar}>
                 <FaBars/>
             </button>
-
         </div>
+
         <ul className="nav-links">
+            <li>
+                <button className='link-btn'>
+                    products
+                </button>
+            </li>
+
+              <li>
+                <button className='link-btn'>
+                    developers
+                </button>
+            </li>
+
+            <li>
+                <button className='link-btn'>
+                   company
+                </button>
+            </li>
 
         </ul>
     </div>
