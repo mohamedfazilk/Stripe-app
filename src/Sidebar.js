320440862f1a83ab7478d30f6bbec78c808487dd
mohamedfazilk <2114921@cognizant.com>
2022-07-26 15:31:39 +0530

    add the template string interpolation into aside class

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useGlobalContext } from './context';
 
 const Sidebar = () => {
-    const {isSidebarOpen, closeSidebar}= useGlobalContext();
-  return (
-    <div>Sidebar</div>
-  )
+    const { isSidebarOpen, closeSidebar } = useGlobalContext();
+    return (
+        <aside className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+            }`}>
+            Sidebar
+        </aside>
+    )
 }
 
 export default Sidebar
