892a2ea267e04a95a77c6a7cf1dd6de90ba1085b
mohamedfazilk <2114921@cognizant.com>
2022-07-26 13:41:45 +0530

    design the nav header and place the nave image

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,9 +7,21 @@ const Navbar = () => {
 
     const {openSidebar,openSubmenu,closeSubmenu} = useGlobalContext();
 
-  return <nav>
+  return <nav className='nav'>
+    <div className="nav-center">
+        <div className="nav-header">
+            <img src={logo} className='nav-logo' alt="stripe" />
+            <button className='btn toggle-btn'>
+                <FaBars/>
+            </button>
 
-      
+        </div>
+        <ul className="nav-links">
+
+        </ul>
+    </div>
+
+
   </nav>
 }
 
