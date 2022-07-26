7d13a47601288aee018d9b777f32c13299b815e4
mohamedfazilk <2114921@cognizant.com>
2022-07-26 16:02:20 +0530

    double iterate in  sublink nested listand pass the index as key

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useGlobalContext } from './context';
 import { FaTimes } from 'react-icons/fa'
+import sublinks from './data';
 
 const Sidebar = () => {
     const { isSidebarOpen, closeSidebar } = useGlobalContext();
@@ -8,9 +9,35 @@ const Sidebar = () => {
         <aside className={`${isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
             }`}>
             <div className="sidebar">
-                <button className='close-btn'>
+                <button className='close-btn' onClick={closeSidebar}>
                     <FaTimes />
                 </button>
+
+                <div className="sidebar-links">
+                    {sublinks.map((item, index) => {
+                        const { page,links} = item; 
+                        //iterating sublink =[  //need double iteration
+                        // {1.page: 2.links:[]}]
+                        return (
+                            <article key={index}>
+                                <h4>{page}</h4>
+                           <div className="sidebar-sublinks">
+                               {links.map((link,index)=>{
+                                   const{url,label,icon} = link;
+                                   return(
+                                       <a key={index} href={url}>
+                                           {icon}
+                                           {label}
+                                       </a>
+                                   )
+                               })}
+                           </div>
+
+                            </article>
+                            
+                        ) 
+                    })}
+                </div>
             </div>
         </aside >
     )
