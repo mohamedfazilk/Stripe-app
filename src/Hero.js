73a70a460998b3d8118be1de0cda1947d673b9ab
mohamedfazilk <2114921@cognizant.com>
2022-07-26 14:09:47 +0530

    design the hero-info article

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
-import {Appcontext,useGlobalContext} from './context';
+import {    useGlobalContext} from './context';
 
 const Hero = () => {
     const {closeSubmenu} = useGlobalContext();
@@ -9,7 +9,14 @@ const Hero = () => {
     <section className='hero'>
         <div className="hero-center">
             <article className="hero-info">
+                <h1>Payments infrastructure <br /> for the internet</h1>
+                <p>
+                Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+                </p>
 
+                <button className='btn'>Start now</button>
             </article>
 
             <article className='hero-images'>
