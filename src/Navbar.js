777e924cc699ccff43bad13f43571f1ab899966d
mohamedfazilk <2114921@cognizant.com>
2022-07-26 22:14:04 +0530

    create a separate handleSubmenu function for checking if the class cannot have link-btn class then invoke the closeSubmenu

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,9 +14,17 @@ const Navbar = () => {
         const bottom = tempBtn.bottom - 3;
         
         openSubmenu(page,{center,bottom});
+        
     }
 
-  return <nav className='nav'>
+    const handleSubmenu = (e) =>{
+        if(!e.target.classList.contains('link-btn'))
+        closeSubmenu();
+    }
+
+    //if the class not containe link-btn then invoke the closeSubmenu() function
+
+  return <nav className='nav' onMouseOver={handleSubmenu}>
     <div className="nav-center">
         <div className="nav-header">
             <img src={logo} className='nav-logo' alt="stripe" />
